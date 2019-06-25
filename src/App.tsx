82ad67aaac3bd60caff6a46f3936e45e6f37e67d
Charlee Li <oda.charlee@gmail.com>
2019-06-25 15:43:36 -0400

    added site footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,18 @@ import {
   Button,
 } from '@material-ui/core';
 import BundlesChecklist, { CheckedState } from './BundlesChecklist';
+import Github from './Github';
 
 const COOKIE_NAME: string = 'svbc_state';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {},
+    root: {
+      background: theme.palette.background.default,
+    },
 
     body: {
       padding: 16,
-      background: theme.palette.background.default,
     },
 
     content: {
@@ -30,6 +32,16 @@ const useStyles = makeStyles((theme: Theme) =>
 
     title: {
       flex: 1,
+    },
+
+    footer: {
+      textAlign: 'center',
+      fontSize: 12,
+      marginTop: 40,
+      borderTop: `1px solid ${theme.palette.divider}`,
+      paddingTop: 24,
+      marginBottom: 48,
+      color: theme.palette.grey[500],
     },
   }),
 );
@@ -70,6 +82,38 @@ const App: React.FC = () => {
             onCheckedStateChange={setCheckedState}
           />
         </div>
+      </div>
+      <div className={classes.footer}>
+        <p>
+          Stardew Valley Bundles Checklist developed by{' '}
+          <a href="https://github.com/charlee" target="_blank">
+            Charlee Li
+          </a>
+          . The source code is licensed under MIT.
+        </p>
+        <p>
+          Bundle and Item information is from the{' '}
+          <a href="https://stardewvalleywiki.com/Bundles" target="_blank">
+            Stardew Valley Wiki
+          </a>{' '}
+          and is used under the{' '}
+          <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank">
+            CC BY-NC-SA 3.0
+          </a>{' '}
+          License.
+        </p>
+        <p>
+          Stardew Valley &copy;{' '}
+          <a href="https://chucklefish.org/" target="_blank">
+            Chucklefish LTD.
+          </a>{' '}
+          Developed by{' '}
+          <a href="https://twitter.com/ConcernedApe" target="_blank">
+            ConcernedApe
+          </a>
+          .
+        </p>
+        <p><Github link="https://github.com/charlee/stardewvalleybundleschecklist"/></p>
       </div>
     </div>
   );
